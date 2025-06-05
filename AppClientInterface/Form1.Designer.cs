9f51c505f9afd8df919f36f03cefc737bab425b2
Semen Domin <deminsem2008@gmail.com>
2025-06-05 15:36:56 +0200

    WinForms Log In

--- AppClientInterface/Form1.Designer.cs
+++ AppClientInterface/Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,26 +23,83 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(240, 259);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Log in";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(240, 84);
+            label1.Name = "label1";
+            label1.Size = new Size(112, 46);
+            label1.TabIndex = 1;
+            label1.Text = "Log in";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(240, 157);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(224, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(240, 217);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(224, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(240, 341);
+            label2.Name = "label2";
+            label2.Size = new Size(12, 20);
+            label2.TabIndex = 4;
+            label2.Text = ".";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
     }
 }
-
--- AppClientInterface/Form1.cs
+++ AppClientInterface/Form1.cs
@@ -1,13 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
 namespace AppClientInterface
 {
     public partial class Form1 : Form
@@ -17,9 +7,26 @@ namespace AppClientInterface
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
 
+            if (username == "admin" && password == "1234")
+            {
+                label2.ForeColor = Color.Green;
+                label2.Text = "Successfully logged in!";
+            }
+            else if (username == "" || password == "")
+            {
+                label2.ForeColor = Color.OrangeRed;
+                label2.Text = "Enter username and password!";
+            }
+            else
+            {
+                label2.ForeColor = Color.Red;
+                label2.Text = "Incorrect username or password!";
+            }
         }
     }
 }
